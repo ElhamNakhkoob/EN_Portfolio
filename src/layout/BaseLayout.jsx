f4cb21e4310ac41c35efa279c4e3a99dc080f10f
ElhamNakhkoob <elham_mn14@yahoo.com>
2025-05-27 14:18:44 +0200

    solved style

--- src/layout/BaseLayout.jsx
+++ src/layout/BaseLayout.jsx
@@ -5,7 +5,7 @@ export default function BaseLayout() {
   return (
     <div className="flex flex-col md:flex-row h-screen">
       <Sidebar />
-      <main className="flex-1 overflow-auto p-6 md:pt-6 bg-[#302E29]">
+      <main className="flex-1 overflow-auto p-6 pt-24 md:pt-6 bg-[#302E29]">
         <Outlet />
       </main>
     </div>
